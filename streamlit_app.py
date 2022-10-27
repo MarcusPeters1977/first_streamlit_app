58e6d12e2d8871f7da7f27c273745e80a2bccac9
Marcus Peters <116799232+MarcusPeters1977@users.noreply.github.com>
2022-10-27 10:24:44 +0100

    Update streamlit_app.py
    Updated menu!

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Full English')
-streamlit.text('Veggie English')
-streamlit.text('Toast and Marmite')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
