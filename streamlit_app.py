8a9fefbf87f7f27b216eee5608d438b8e46111d2
Marcus Peters <116799232+MarcusPeters1977@users.noreply.github.com>
2022-10-27 10:23:11 +0100

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Full English')
+streamlit.text('Veggie English')
+streamlit.text('Toast and Marmite')
